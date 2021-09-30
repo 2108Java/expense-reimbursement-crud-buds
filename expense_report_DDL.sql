11c91d2e01573bc4d9c469e1b49254833161fe98
brandonzamora01 <89804779+brandonzamora01@users.noreply.github.com>
2021-09-30 11:09:25 -0400

    Adding DDL script V1

---
+++ expense_report_DDL.sql
@@ -0,0 +1,18 @@
+create table employee (
+	employee_id serial primary key,
+	username varchar(20) not null unique,
+	password varchar(20) not null,
+	is_finance_manager boolean not null,
+	--email varchar(50) not null unique, only used for strech goal--
+
+);
+
+create table expense_reports (
+	report_id serial primary key,
+	amount numeric not null,
+	expense_type varchar(10) not null,--4 states are lodging, travel, food, or other-- 
+	creation_time timestamp not null, --date format example '2004-10-19 10:23:54'--
+	approval_status varchar(10) default 'Pending', --3 states are pending, approved, or denied--
+	employee_id int FOREIGN KEY REFERENCES employee(employee_id),
+	
+);
