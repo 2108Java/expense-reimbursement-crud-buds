d7c5d974deabda241c37baf7f90315f525ec9e66
Cavemanhar <harley.burch@revature.net>
2021-09-30 10:48:43 -0500

    fixing sql table statements

--- expense_report_DDL.sql
+++ expense_report_DDL.sql
@@ -2,7 +2,7 @@ create table employee (
 	employee_id serial primary key,
 	username varchar(20) not null unique,
 	password varchar(20) not null,
-	is_finance_manager boolean not null,
+	is_finance_manager boolean not null
 	--email varchar(50) not null unique, only used for strech goal--
 
 );
@@ -13,6 +13,6 @@ create table expense_reports (
 	expense_type varchar(10) not null,--4 states are lodging, travel, food, or other-- 
 	creation_time timestamp not null, --date format example '2004-10-19 10:23:54'--
 	approval_status varchar(10) default 'Pending', --3 states are pending, approved, or denied--
-	employee_id int FOREIGN KEY REFERENCES employee(employee_id),
+	employee_id int REFERENCES employee(employee_id)
 	
 );
