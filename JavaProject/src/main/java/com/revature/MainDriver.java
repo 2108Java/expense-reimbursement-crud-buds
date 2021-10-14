9cc1f594e490094b2c0f61cc184dea78c75b24b8
brandonzamora01 <89804779+brandonzamora01@users.noreply.github.com>
2021-10-14 10:20:04 -0400

    Update MainDriver.java

--- JavaProject/src/main/java/com/revature/MainDriver.java
+++ JavaProject/src/main/java/com/revature/MainDriver.java
@@ -14,14 +14,17 @@ import io.javalin.Javalin;
 
 public class MainDriver {
 
-		Javalin app = Javalin.create(config -> config.addStaticFiles(
-				staticFiles ->
-				{
-					staticFiles.directory = "/public";
-				}
-				)).start(8000);
+
+	public static void main(String[] args) {
 		
-		RequestHandler.setupEndPoints(app);	 
+		 Javalin app = Javalin.create(config -> config.addStaticFiles(
+					staticFiles ->
+					{
+						staticFiles.directory = "/public";
+					}
+					)).start(8000);
+		 
+		 RequestHandler.setupEndPoints(app);
 	}
 
 }
